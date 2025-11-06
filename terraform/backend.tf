ef23484e634b2a83b35f2f5dfb0709fc428ba0a4
palegavr <palegavr@gmail.com>
2025-11-06 13:03:24 +0200

    Update S3 bucket name in backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-jkbskefbjek"
+    bucket       = "terraform-tfstate-jkbskefbjeksadfga"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
